934d8804e41235c200ed5352f0445bfa6c07866d
janhavi73 <janhavisinghania0607@gmail.com>
2026-01-30 16:31:44 +0000

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -1,17 +1,23 @@
-import pygame
-pygame.init()
-screen = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("my first game screen")
-bg_color = (58, 58, 58)
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    screen.fill(bg_color)
-    
-    
-    
-    pygame.display.flip()
+class RomanConverter:
+    def int_to_roman(self, num):
+        
+        roman_map = [
+            (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+            (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+            (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
+        ]
 
-pygame.quit()
+        result = []
+        for value, numeral in roman_map:
+            count = num // value
+            if count > 0:
+                result.append(numeral * count)
+                num -= value * count
+            if num == 0:
+                break
+
+        return "".join(result)
+user_input = int(input("enter a number from (1-3999): "))
+converter = RomanConverter()
+roman_value = converter.int_to_roman(user_input)
+print(f"The Roman numeral for {user_input} is: {roman_value}")
